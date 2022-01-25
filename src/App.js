b95f92097194fcbd135e76dd6e010a0e59e17dfa
arpanp11 <arpanpatel2810@gmail.com>
2022-01-25 11:48:51 -0500

    pages and route setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Explore from './pages/Explore';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
   return (
     <>
-      <h1>Dream Home</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/forgotpassword' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
