9ee113cca88fff15797a8fdd280ea4e8f1f867a1
arpanp11 <arpanpatel2810@gmail.com>
2022-01-26 09:39:51 -0500

    added signin and signup page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,88 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import VisibilityIcon from '../assets/svg/visibilityIcon.svg';
+
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const changeHandler = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+
+        <form>
+          <input
+            type='text'
+            placeholder='Name'
+            id='name'
+            value={name}
+            onChange={changeHandler}
+            className='nameInput'
+          />
+          <input
+            type='email'
+            placeholder='Email'
+            id='email'
+            value={email}
+            onChange={changeHandler}
+            className='emailInput'
+          />
+          <div className='passwordInputDiv'>
+            <input
+              type={showPassword ? 'text' : 'password'}
+              id='password'
+              placeholder='Password'
+              value={password}
+              onChange={changeHandler}
+              className='passwordInput'
+            />
+
+            <img
+              src={VisibilityIcon}
+              alt='showpassword'
+              className='showPassword'
+              onClick={() => setShowPassword((prevState) => !prevState)}
+            />
+          </div>
+
+          <Link to='/forgot-password' className='forgotPasswordLink'>
+            Forgot Password
+          </Link>
+
+          <div className='signUpBar'>
+            <p className='signUpText'>Sign Up</p>
+            <button className='signUpButton'>
+              <ArrowRightIcon fill='#ffffff' width='34px' height='34px' />
+            </button>
+          </div>
+        </form>
+
+        {/* google OAuth */}
+
+        <Link to='/sign-in' className='registerLink'>
+          Sign In
+        </Link>
+      </div>
+    </>
   );
 }
 
