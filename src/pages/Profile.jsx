062c1d00f72a5634817042dad87f06d7883d7d12
arpanp11 <arpanpatel2810@gmail.com>
2022-01-27 11:39:10 -0500

    fetch listing from db

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
 import { updateDoc, doc } from 'firebase/firestore';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import { db } from '../firebase.config';
