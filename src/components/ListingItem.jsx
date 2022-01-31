a94ea2c5d2fe9202bc5b6cba682efd9364adf007
arpanp11 <arpanpatel2810@gmail.com>
2022-01-31 11:07:36 -0500

    updated profile page and fixed bedroom icon

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg';
-import bedIcon from '../assets/svg/badgeIcon.svg';
+import bedIcon from '../assets/svg/bedIcon.svg';
 import bathtubIcon from '../assets/svg/bathtubIcon.svg';
 
 function ListingItem({ listing, id, onDelete }) {
